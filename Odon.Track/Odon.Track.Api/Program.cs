f9903850bc9a09fc7ea278b75529d2a6770243a3
João Marcelo <joaomarcelobatistanarciso@gmail.com>
2024-05-06 09:46:00 -0300

    teste

--- Odon.Track/Odon.Track.Api/Program.cs
+++ Odon.Track/Odon.Track.Api/Program.cs
@@ -1,48 +1,50 @@
 using Odon.Track.Application.Configuration;
 using Odon.Track.Application.Core.Middleware;
 using Odon.Track.Application.Core.Injections.Extensions;
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-var services = builder.Services;    
+var services = builder.Services;
 IConfiguration configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .Build();
 
 AppSettings appSettings = new AppSettings(configuration);
 
-services.AddCommon(configuration);
+services.AddScoped<AppSettings>();
 services.AddServices();
-services.AddContexts(appSettings);
-services.AddEndpointsApiExplorer();
-services.AddCustomCors(appSettings);
-services.AddCustomControllers();
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
-services.AddSwaggerGen(c =>
+services.AddLogging();
+
+
+
+services.AddControllers(config =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OdonTrack API", Version = "v1" });
-});
-services.AddAuthorization();
-services.AddAuthentication("Bearer").AddJwtBearer();
+    config.Filters.Add(new ConsumesAttribute("application/json"));
+    config.Filters.Add(new ProducesAttribute("application/json"));
+})
+                    .AddJsonOptions(
+                          config =>
+                          {
+                              config.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                          });
+
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OdonTrack API v1");
-        c.RoutePrefix = string.Empty; // Define a rota para a raiz da aplicação
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseExceptionHandler(error => error.UseCustomError());
 app.UseAuthorization();
+app.MapControllers();
 app.UseCors(x =>
             x.AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod());
-app.MapControllers();
 app.Run();
