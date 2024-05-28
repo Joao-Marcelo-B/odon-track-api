62abd298f5f2a264f0d36b7bdd2ec704eb5ea347
Joao-Marcelo-WMoney <joao.narciso@wealthmoney.com.br>
2024-05-27 22:13:12 -0300

    feat: colocando idUsuario no token

---
+++ Odon.Track/Odon.Track.Application/GetHeader/GetHeaderValues.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Odon.Track.Application.Configuration;
+using Odon.Track.Application.Crypto;
+using Org.BouncyCastle.Asn1.Ocsp;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Odon.Track.Application.GetToken
+{
+    public class GetHeaderValues()
+    {
+        public static int GetIdUsuario(IEnumerable<Claim> claims)
+        {
+            var idUsuarioHash = claims.First(claim => claim.Type == ClaimTypes.NameIdentifier);
+            if(idUsuarioHash == null)
+                return 0;
+
+            int idUsuario = int.Parse(EncryptionHelper.Decrypt(idUsuarioHash.Value));
+
+            return idUsuario;
+
+        }
+    }
+}
