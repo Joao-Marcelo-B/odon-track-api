001c8bfd53002c94dd2bd82be7aecf2d05928494
João Marcelo <joaomarcelobatistanarciso@gmail.com>
2024-06-24 22:36:04 -0300

    fix: key na reavaliacao de anamnese

--- Odon.Track/Odon.Track.Application/Data/MySQL/Entity/ReavaliacaoAnamnese.cs
+++ Odon.Track/Odon.Track.Application/Data/MySQL/Entity/ReavaliacaoAnamnese.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Odon.Track.Application.Data.MySQL.Entity;
 
 [Table("reavaliacao_anamnese")]
 public partial class ReavaliacaoAnamnese
 {
-    [Column("id")]
+    [Key, Column("id")]
     public int Id { get; set; }
 
     [Column("id_professor_responsavel")]
