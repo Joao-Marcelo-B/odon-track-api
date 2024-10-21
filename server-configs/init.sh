850d0b263ca7d36dee64b04f0e123d5fef951f06
Joao-Marcelo-B <joaomarcelobatistanarciso@gmail.com>
2024-10-21 19:28:50 -0300

    alterando script init.sh

--- server-configs/init.sh
+++ server-configs/init.sh
@@ -11,30 +11,30 @@ fi
 if netstat -tuln | grep ':4200 ' &>/dev/null; then
     echo "A porta 4200 já está em uso."
 else
-    node /home/joaom/Proxy/proxy-frontend.js &
+    node "$HOME/server-configs/proxy/proxy-frontend.js" &
     disown
 fi
 
 if netstat -tuln | grep ':8080 ' &>/dev/null; then
     echo "A porta 8080 já está em uso."
 else
-    node /home/joaom/Proxy/proxy-backend.js &
+    node "$HOME/server-configs/proxy/proxy-backend.js" &
     disown
 fi
 
 if netstat -tuln | grep ':3307' &>/dev/null; then
     echo "A porta 3307 já está em uso."
 else
-    node /home/joaom/Proxy/proxy-mysql.js &
+    node "$HOME/server-configs/proxy/proxy-mysql.js" &
     disown
 fi
 if netstat -tuln | grep ':3000' &>/dev/null; then
     echo "A porta 3000 já está em uso."
 else
-    node /home/joaom/Proxy/proxy-ngrok.js &
+    node "$HOME/server-configs/proxy/proxy-ngrok.js" &
     disown
-    /home/joaom/ngrok.sh &
+    #/home/joaom/ngrok.sh &
 fi
 
-nohup ngrok http --domain=allowing-killdeer-wise.ngrok-free.app 3000 &
+#nohup ngrok http --domain=allowing-killdeer-wise.ngrok-free.app 3000 &
 echo "=============================================="
