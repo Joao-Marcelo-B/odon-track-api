21cccaf1b337083bb90aa597538801b4782651a7
Joao-Marcelo-B <joaomarcelobatistanarciso@gmail.com>
2025-04-25 03:31:33 +0100

    feat: adicionando envs no arquivo

---
+++ server-configs/envs.sh
@@ -0,0 +1,4 @@
+export PathOdonTrackApi="/home/joaom/odon-track-api/Odon.Track/Odon.Track.Api"
+export PathOdonTrackApiPython="/home/joaom/odon-track-api/Odon.Track/Odon.Track.Python.API"
+export PathOdonTrackWeb="/home/joaom/odon-track"
+export PathOdonTrackMySQL="/home/joaom/odon-track-api/server-configs/mysql/OdonTrackMySQL"
