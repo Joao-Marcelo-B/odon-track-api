49306bae564a0a7db696fad1c0bad1b389576005
Joao-Marcelo-B <joaomarcelobatistanarciso@gmail.com>
2025-04-30 16:53:03 +0100

    fix: editando deploy

--- server-configs/scripts/front/frontend-deploy.sh
+++ server-configs/scripts/front/frontend-deploy.sh
@@ -3,5 +3,13 @@
 cd "$PathOdonTrackWeb"
 git checkout master
 git pull
-docker build -t app-web-odon-track-image "$PathOdonTrackWeb"
+
+# Obtém o hash do último commit (curto)
+GIT_COMMIT_HASH=$(git -C "$PathOdonTrackWeb" rev-parse --short HEAD)
+
+docker build -t app-web-odon-track-image:"$GIT_COMMIT_HASH" "$PathOdonTrackWeb"
+docker tag app-web-odon-track-image:"$GIT_COMMIT_HASH" app-web-odon-track-image:latest
+
+#kubectl set image deployment/app-web-odon-track app-web=app-web-odon-track-image:$GIT_COMMIT_HASH
+
 kubectl delete pod -l app=app-web-odon-track
