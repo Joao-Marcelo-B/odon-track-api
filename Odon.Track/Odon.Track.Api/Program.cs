5a4b86436091785fbbdeb5db145c9571344f1640
João Marcelo Batista Narciso <113398296+Joao-Marcelo-B@users.noreply.github.com>
2025-05-28 21:49:38 -0300

    Update Program.cs with swagger configs

--- Odon.Track/Odon.Track.Api/Program.cs
+++ Odon.Track/Odon.Track.Api/Program.cs
@@ -32,25 +32,57 @@ services.AddSwaggerGen();
 services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "OdonTrack API", Version = "v1" });
+
+    // Configuração para autenticação Bearer
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Scheme = "Bearer",
+        BearerFormat = "JWT", // ou qualquer outro formato do seu token
+        Type = SecuritySchemeType.ApiKey,
+        Description = "Insira o token JWT de acesso"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
 });
+
 services.AddCustomAuthorization();
 services.AddCustomAuthentication(appSettings);
 
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OdonTrack API v1");
-        c.RoutePrefix = "swagger";
-        c.ConfigObject.DisplayRequestDuration = true;
-        c.ConfigObject.DocExpansion = Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None;
-        c.ConfigObject.DefaultModelsExpandDepth = -1;
-    });
-}
+c.SwaggerEndpoint("/swagger/v1/swagger.json", "OdonTrack API v1");
+c.RoutePrefix = "swagger";
+c.ConfigObject.DisplayRequestDuration = true;
+c.ConfigObject.DocExpansion = Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None;
+c.ConfigObject.DefaultModelsExpandDepth = -1;
+
+// Habilitar a opção para passar o token Bearer diretamente no Swagger UI
+c.ConfigObject.Authorization = new OpenApiOAuthFlow
+{
+AuthorizationUrl = new Uri("https://seu-servidor-de-autorizacao.com"),
+TokenUrl = new Uri("https://seu-servidor-de-token.com")
+};
+});
+//}
 app.UseRouting();
 app.UseExceptionHandler(error => error.UseCustomError());
 app.UseAuthentication();
