35aa0408f4bff53d79a71823b2f884cc7ab99065
Felipe <fellipearraujjo2004@gmail.com>
2025-06-04 17:42:45 -0300

    fix: program

--- Odon.Track/Odon.Track.Api/Program.cs
+++ Odon.Track/Odon.Track.Api/Program.cs
@@ -1,57 +1,66 @@
-using Odon.Track.Application.Configuration;
-using Odon.Track.Application.Core.Middleware;
-using Odon.Track.Application.Core.Injections.Extensions;
+using System.Reflection;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
-
+using Odon.Track.Application.Configuration;
+using Odon.Track.Application.Core.Injections.Extensions;
+using Odon.Track.Application.Core.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
-IConfiguration configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            .Build();
 
-AppSettings appSettings = new AppSettings(configuration);
+// Carregar configuração a partir do arquivo appsettings.json
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
 
+var appSettings = new AppSettings(configuration);
+
+// Configuração básica de serviços
 services.AddLogging();
+
 services.AddCors(options =>
 {
-    options.AddDefaultPolicy(
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
+
 services.AddServices();
 services.AddContexts(appSettings);
 services.AddCustomControllers();
 services.AddEndpointsApiExplorer();
+
+// Configuração do Swagger
 services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo()
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "Catalogo API",
+        Title = "Catálogo API",
         Version = "v1",
-        Description = "Catalogo de Produtos e Categorias",
+        Description = "Catálogo de Produtos e Categorias",
         TermsOfService = new Uri("https://google.com"),
-        Contact = new OpenApiContact()
+        Contact = new OpenApiContact
         {
             Name = "Joao Marcelo",
             Email = "joaomarcelobn157@hotmail.com",
             Url = new Uri("https://google.com")
         },
-        License = new OpenApiLicense()
+        License = new OpenApiLicense
         {
             Name = "License",
             Url = new Uri("https://google.com")
         }
     });
 
+    // Comentários XML
     var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+    var xmlFilePath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
+    c.IncludeXmlComments(xmlFilePath);
 
+    // Segurança JWT
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -61,6 +70,7 @@ services.AddSwaggerGen(c =>
         In = ParameterLocation.Header,
         Description = "Bearer JWT"
     });
+
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
@@ -72,30 +82,35 @@ services.AddSwaggerGen(c =>
                     Id = "Bearer"
                 }
             },
-            new string[] { }
+            Array.Empty<string>()
         }
     });
 });
 
-
+// Autenticação e Autorização
 services.AddCustomAuthorization();
 services.AddCustomAuthentication(appSettings);
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+// Middlewares e Pipeline
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseRouting();
+
 app.UseExceptionHandler(error => error.UseCustomError());
+
 app.UseAuthentication();
 app.UseCookiePolicy();
 app.UseAuthorization();
+
+app.UseCors(policy =>
+    policy.AllowAnyOrigin()
+          .AllowAnyHeader()
+          .AllowAnyMethod()
+);
+
 app.MapControllers();
-app.UseCors(x =>
-            x.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod());
+
 app.Run();
